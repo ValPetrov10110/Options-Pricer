79ed97db49ad42dc0d07cb6f9f8cc2aba9ece38a
Valentin Petrov <valpetrov.soccer@gmail.com>
2026-03-14 18:06:15 -0400

    Completed the base BlackScholes implementation. Not tested. Working on implementing the setters to make it more flexible

--- include/pricing/BlackScholes.hpp
+++ include/pricing/BlackScholes.hpp
@@ -8,6 +8,14 @@ class BlackScholes{
 		double r;
 		double sigma;
 		double T;
+		double d1;
+		double d2;
+
+		double calculate_d1();
+		double calculate_d2();
+		double calculateOption();
+		double standardNormalCDF(double d);
+		void recalculate();
 
 	public:
 		BlackScholes(double S, double K, double r, double sigma, double T);
--- src/pricing/BlackScholes.cpp
+++ src/pricing/BlackScholes.cpp
@@ -1,9 +1,36 @@
 #include "../../include/pricing/BlackScholes.hpp"
+#include <cmath>
 
+// The constructor for the BlackScholes model implementation
 BlackScholes::BlackScholes(double S, double K, double r, double sigma, double T){
-	S = this->S;
-	K = this->K;
-	r = this->r;
-	sigma = this->sigma;
-	T = this->T;
+	this->S = S;
+	this->K = K;
+	this->r = r;
+	this->sigma = sigma;
+	this->T = T;
+
+	d1 = calculate_d1();
+	d2 = calculate_d2();
+}
+
+double BlackScholes::calculate_d1(){
+	return (std::log(S/K)+(r+std::pow(sigma, 2))*T) / (sigma*std::sqrt(T));
+}
+
+double BlackScholes::calculate_d2(){
+	return d1 - sigma*std::sqrt(T);
+}
+
+double BlackScholes::standardNormalCDF(double d){
+	return 0.5 * (1.0 + std::erf(d / std::sqrt(2.0)));
+}
+
+void BlackScholes::recalculate(){
+	d1 = calculate_d1();
+	d2 = calculate_d2();
+}
+
+double BlackScholes::calculateOption(){
+	// Formula: C = SN(d1) - Ke^(-rt)N(d2)
+	return S*standardNormalCDF(d1) - K*std::pow(std::exp(1.0), -r*T)*standardNormalCDF(d2);
 }
