c706e35ba56f335a38f191a2955e3b0a0a1986fe
Valentin Petrov <valpetrov.soccer@gmail.com>
2026-03-15 17:54:51 -0400

    Modified BlackScholes. Implemented a debug method to check individual variables

--- TESTING/main.cpp
+++ TESTING/main.cpp
@@ -5,8 +5,18 @@
 int main(){
 	BlackScholes calculation{}; // Utilizing the default constructor
 	BlackScholes myCalc{70, 100, 0.05, 0.2, 1}; // Initialized with values
+	double call{};
+	double put{};
 
-	std::cout << calculation.calculateOption() << '\n';
-	std::cout << myCalc.calculateOption() << '\n';
+	calculation.loadValues(100, 100, 0.05, 0.20, 1);
+	call = calculation.calculateCallOption();
+	put = calculation.calculatePutOption();
+
+	std::cout << call << " " << put << '\n';
+
+	// Test 2
+	calculation.loadValues(110, 90, 0.05, 0.20, 1.0);
+	calculation.debug();
+
 	return EXIT_SUCCESS;
 }
