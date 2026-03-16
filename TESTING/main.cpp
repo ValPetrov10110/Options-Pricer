33003f88175cfde62e49015b8733f3992b4ad92e
Valentin Petrov <valpetrov.soccer@gmail.com>
2026-03-16 12:20:03 -0400

    Modified the testing section

--- TESTING/main.cpp
+++ TESTING/main.cpp
@@ -7,6 +7,8 @@ int main(){
 	MonteCarlo test{184.14, 185, 0.0427, .3909, 32/365.0, 1000000};
 	std::cout << test.calculateCallOption() << " " << test.calculatePutOption() << '\n'; 
 
+	BlackScholes testing{5.03, 6, .01, .65, 148.0/365};
+	std::cout << testing.calculateCallOption();
 
 	return EXIT_SUCCESS;
 }
