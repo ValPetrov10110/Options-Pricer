e276b0e9f7f9a2d6973f99b29123ee64848a2d5e
Valentin Petrov <valpetrov.soccer@gmail.com>
2026-03-17 17:55:29 -0400

    Test

--- TESTING/main.cpp
+++ TESTING/main.cpp
@@ -4,10 +4,10 @@
 #include <iostream>
 
 int main(){
-	MonteCarlo test{184.14, 185, 0.0427, .3909, 32/365.0, 1000000};
+	MonteCarlo test{181.98, 190, 0.0427, .3389, 32/365.0, 1000000};
 	std::cout << test.calculateCallOption() << " " << test.calculatePutOption() << '\n'; 
 
-	BlackScholes testing{5.03, 6, .01, .65, 148.0/365};
+	BlackScholes testing{181.98, 190, 0.0427, .3389, 33/365.0};
 	std::cout << testing.calculateCallOption();
 
 	return EXIT_SUCCESS;
