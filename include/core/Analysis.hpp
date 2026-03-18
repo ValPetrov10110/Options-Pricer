327cddde4cede8233a488efc47da1cf8e45a15fd
ValPetrov10110 <valpetrov.soccer@gmail.com>
2026-03-17 22:58:17 -0400

    Completed Most of the analysis class

--- include/core/Analysis.hpp
+++ include/core/Analysis.hpp
@@ -7,8 +7,16 @@ class Analysis{
 private:
 	BlackScholes bs;
 	MonteCarlo mc;
+	double bsRuntime;
+	double mcRuntime;
 
 	void validate();
+	void testRuntime();
+	void testBlackScholesRuntime();
+	void testMonteCarloRuntime();
 public:
 	Analysis(BlackScholes bsModel, MonteCarlo mcModel);
+	void printResults();
+	double getBlackScholesRuntime();
+	double getMonteCarloRuntime();
 };
--- src/core/Analysis.cpp
+++ src/core/Analysis.cpp
@@ -1,10 +1,59 @@
 #include "../../include/core/Analysis.hpp"
 
+#include <chrono>
+#include <cmath>
+
 Analysis::Analysis(BlackScholes bsModel, MonteCarlo mcModel) : bs(bsModel), mc(mcModel){
+	validate();
+	testRuntime();
 }
 
 void Analysis::validate(){
-	if(){
-
+	if(bs.getStock() != mc.getStock() || bs.getStrikePrice() != mc.getStrikePrice() || bs.getInterestRate() != mc.getInterestRate() || bs.getVolatility() != mc.getVolatility() || bs.getTime() != mc.getTime()){
+		throw std::invalid_argument("ERROR: Different BlackScholes and MonteCarlo variables");
 	}
+}
+
+void Analysis::testRuntime(){
+	testBlackScholesRuntime();
+	testMonteCarloRuntime();
+}
+
+void Analysis::testBlackScholesRuntime(){
+	auto startTime = std::chrono::steady_clock::now();
+
+	bs.calculateCallOption();
+	bs.calculatePutOption();
+
+	auto endTime = std::chrono::steady_clock::now();
+	auto diff = endTime - startTime;
+
+	bsRuntime =  std::chrono::duration<double, std::milli>(diff).count();
+}
+
+void Analysis::testMonteCarloRuntime(){
+	auto startTime = std::chrono::steady_clock::now();
+	
+	mc.calculateCallOption();
+	mc.calculatePutOption();
+
+	auto endTime = std::chrono::steady_clock::now();
+	auto diff = endTime - startTime;
+	
+	mcRuntime = std::chrono::duration<double, std::milli>(diff).count();
+}
+
+void Analysis::printResults(){
+	std::cout << "BlackScholes runtime: " << bsRuntime << "ms\n";
+	std::cout << "MonteCarlo runtime: " << mcRuntime << "ms\n";
+	std::cout << "Difference: " << std::abs(bsRuntime - mcRuntime) << "ms\n";
+	std::cout << "Faster model: " << (bsRuntime < mcRuntime ? "BlackScholes" : "MonteCarlo") << "\n";
+}
+
+double Analysis::getBlackScholesRuntime(){
+	return bsRuntime;
+}
+
+double Analysis::getMonteCarloRuntime(){
+	return mcRuntime;
 }
