29b3b10615e26b0c25e8638e017e6e1e0722a019
ValPetrov10110 <valpetrov.soccer@gmail.com>
2026-03-17 22:58:07 -0400

    Completed Most of the analysis class

--- TESTING/main.cpp
+++ TESTING/main.cpp
@@ -1,14 +1,28 @@
 #include "../src/pricing/BlackScholes.cpp"
 #include "../src/pricing/MonteCarlo.cpp"
+#include "../src/core/Analysis.cpp"
 #include <cstdlib>
 #include <iostream>
 
 int main(){
-	MonteCarlo test{181.98, 190, 0.0427, .3389, 32/365.0, 1000000};
-	std::cout << test.calculateCallOption() << " " << test.calculatePutOption() << '\n'; 
 
-	BlackScholes testing{181.98, 190, 0.0427, .3389, 33/365.0};
-	std::cout << testing.calculateCallOption();
+	BlackScholes bs{100, 100, .0437, .2, 1};
+	MonteCarlo mc{100, 100, .0437, .2, 1, 1000000};
+	double blackRT{};
+	double mcRT{};
+
+	try{
+		Analysis test{bs, mc};
+		test.printResults();
+		blackRT = test.getBlackScholesRuntime();
+		mcRT = test.getMonteCarloRuntime();
+	}catch(std::invalid_argument e){
+		std::cout << e.what() << '\n';
+		return EXIT_FAILURE;
+	}
+
+	std::cout << "\nBlackScholes Runtime: " << blackRT << "ms\n";
+	std::cout << "MonteCarlo Runtime: " << mcRT << "ms\n";
 
 	return EXIT_SUCCESS;
 }
